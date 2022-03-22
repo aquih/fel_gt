69f773f94de73cbda7f2bba5d72a29789c5a3fcd
rocketgithub <rodrigo@aquih.com>
2022-03-22 12:37:07 -0600

    No formato de factura, en la 11 no funciona

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     'data': [
         'views/account_view.xml',
         'views/partner_view.xml',
-        'views/report_invoice.xml',
+        #'views/report_invoice.xml',
     ],
     'demo': [],
     'installable': True
