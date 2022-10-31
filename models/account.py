c028e3792a105cc6dc8247c0befac8164387b79a
rocketgithub <rodrigo@aquih.com>
2022-10-31 11:47:37 -0600

    Si era primero de noviembre

--- models/account.py
+++ models/account.py
@@ -207,7 +207,7 @@ class AccountMove(models.Model):
         ElementoFrases = None
         if tipo_documento_fel not in ['NABN', 'FESP']:
             ElementoFrases = etree.fromstring(factura.company_id.frases_fel)
-            if datetime.now().isoformat() < '2022-10-31' and tipo_documento_fel not in ['FACT', 'FCAM']:
+            if datetime.now().isoformat() < '2022-11-01' and tipo_documento_fel not in ['FACT', 'FCAM']:
                 frase_isr = ElementoFrases.find('.//*[@TipoFrase="1"]')
                 if frase_isr is not None:
                     ElementoFrases.remove(frase_isr)
