d56bdefe22e8afa4344d1d577d22a4624e5df67b
rocketgithub <rodrigo@aquih.com>
2022-12-06 12:42:47 -0600

    Poner CUI

--- models/account.py
+++ models/account.py
@@ -157,6 +157,8 @@ class AccountInvoice(models.Model):
             Receptor.attrib['NombreReceptor'] = factura.partner_id.nombre_facturacion_fel
         if factura.partner_id.email:
             Receptor.attrib['CorreoReceptor'] = factura.partner_id.email
+        if len(nit_receptor) > 9:
+            Receptor.attrib['TipoEspecial'] = "CUI"
         if tipo_documento_fel == "FESP" and factura.partner_id.cui:
             Receptor.attrib['TipoEspecial'] = "CUI"
         if tipo_documento_fel == "FESP" and factura.tipo_gasto == 'importacion':
