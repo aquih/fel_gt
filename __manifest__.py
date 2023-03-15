2b7831d2f0497abdec6eaa01244ed25be7f0ae03
Rodrigo Fernández <rodrigo@aquih.com>
2023-03-15 16:31:38 +0000

    Aumentar versión

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'FEL Guatemala',
-    'version': '1.2',
+    'version': '1.3',
     'category': 'Custom',
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'aquíH',
