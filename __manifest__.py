b6f76d4b8222aa1e131e243155a231b3681d3a43
rocketgithub <rodrigo@aquih.com>
2024-07-11 11:49:24 -0600

    Actualizar versión

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'FEL Guatemala',
-    'version': '1.3',
+    'version': '1.4',
     'category': 'Custom',
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'aquíH',
