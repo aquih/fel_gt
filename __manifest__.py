4b53416dc0b3767fe125d777c360479ead08d514
rocketgithub <rodrigo@aquih.com>
2024-07-12 11:55:42 -0600

    Por estar cambiando res_partner es necesario depender de base también

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'aquíH',
     'website': 'http://aquih.com/',
-    'depends': ['l10n_gt_extra'],
+    'depends': ['base', 'l10n_gt_extra'],
     'data': [
         'views/account_view.xml',
         'views/res_company_view.xml',
