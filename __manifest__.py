ec013839faa2a5b952c946ede25ef6b90aeea720
rocketgithub <rodrigo@aquih.com>
2026-04-30 21:49:51 -0600

    Faltaba cerrar versión.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'FEL Guatemala',
-    'version': '1.17,
+    'version': '1.17',
     'category': 'Custom',
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'aquíH',
