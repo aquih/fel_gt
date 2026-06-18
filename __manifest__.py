5497535d5a65eea0b98d48c0f6bc4900d1d3eb68
rocketgithub <rodrigo@aquih.com>
2026-06-18 17:08:09 -0600

    Enviar descripción correcta a FEL

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'FEL Guatemala',
-    'version': '1.21',
+    'version': '1.22',
     'category': 'Custom',
     'description': """ Campos y funciones base para la facturación electrónica en Guatemala """,
     'author': 'aquíH',
